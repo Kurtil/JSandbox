32590d085575860976240f68f264afaaa4240536
g.lagier <gaetan.lagier@hotmail.fr>
2018-09-08 00:20:59 +0200

    add some comment after reading about tasks/microtasks

--- JS-Basics/Promises/test.js
+++ JS-Basics/Promises/test.js
@@ -10,3 +10,7 @@ Promise.resolve().then(() =>
     console.log(2)).then(() =>
         console.log(3));
 console.log(1);
+
+// This example show the difference between microtasks and task.
+// Settimout leads to a task
+// then() leads to microtasks
