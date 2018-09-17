d84a48b5a5588d7bfbde712d5fe34c6b648be6a7
g.lagier <gaetan.lagier@hotmail.fr>
2018-09-17 13:30:22 +0200

    add jsBasics proxy

---
+++ JS-Basics/Proxy/proxy.js
@@ -0,0 +1,24 @@
+// the handler is an object with "traps" that can redefine basic action like get
+// which redefin the access to properties
+const handler = {
+    get(obj, prop) {
+        if (prop in obj) {
+            return obj[prop];
+        } else {
+            return "Property not defined.";
+        }
+    }
+};
+
+const obj = {
+    name: "Jean",
+    age: 32,
+    hobbies: ["surf", "traveling"]
+};
+
+const proxy = new Proxy(obj, handler);
+
+console.log(proxy.name);
+console.log(proxy.age);
+console.log(proxy.hobbies);
+console.log(proxy.unknownProp); // This one is interesting ;)
