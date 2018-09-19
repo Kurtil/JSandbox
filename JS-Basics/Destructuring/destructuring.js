3cdbb446fa4bd763c7691b8fe47b73450f6ecb43
LAGIER Gaetan <gaetan.lagier@viveris.fr>
2018-09-19 10:29:45 +0200

    add comment in destructuring - js basics

--- JS-Basics/Destructuring/destructuring.js
+++ JS-Basics/Destructuring/destructuring.js
@@ -3,7 +3,7 @@ const foo = ({ arg: { a = 2, b = 3, c = 4 } }) => {
     console.log(a,b,c);
 }
 
-// foo({}); // destructuring error
+// foo({}); // destructuring error... see default value to handle this case safely
 
 foo({arg : {}}); // will log the default values
 foo({arg : {a: 10, b:"coucou", c : "yo"}}); // will log the argument values
