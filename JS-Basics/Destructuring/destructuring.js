0636d1a7292730a4caaa1c7da1738925c7f62d31
LAGIER Gaetan <gaetan.lagier@viveris.fr>
2018-09-19 10:10:09 +0200

    add destructuring in js basics

---
+++ JS-Basics/Destructuring/destructuring.js
@@ -0,0 +1,9 @@
+// Warning will destructuring : in this example, argument object have to have an arg property!
+const foo = ({ arg: { a = 2, b = 3, c = 4 } }) => {
+    console.log(a,b,c);
+}
+
+// foo({}); // destructuring error
+
+foo({arg : {}}); // will log the default values
+foo({arg : {a: 10, b:"coucou", c : "yo"}}); // will log the argument values
