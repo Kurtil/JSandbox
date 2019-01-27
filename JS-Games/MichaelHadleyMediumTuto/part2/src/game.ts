8f97ec5d1ad8a7521aee11744c91543073fb4f1b
g.lagier <gaetan.lagier@hotmail.fr>
2019-01-27 11:34:28 +0100

    finish part 2 of phaser modular game tuto

---
+++ JS-Games/MichaelHadleyMediumTuto/part2/src/game.ts
@@ -0,0 +1,21 @@
+import * as Phaser from 'phaser';
+import MainScene from './scenes/mainScene';
+import PlatformerScene from './scenes/mainScene'
+
+const config: GameConfig = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: "game-container",
+    pixelArt: true,
+    backgroundColor: "#1d212d",
+    scene: PlatformerScene,
+    physics: {
+      default: "arcade",
+      arcade: {
+        gravity: { y: 1000 }
+      }
+    }
+};
+
+const game = new Phaser.Game(config);
---
+++ JS-Games/MichaelHadleyMediumTuto/part2/src/scenes/player.ts
@@ -0,0 +1,106 @@
+import * as Phaser from "phaser";
+
+/**
+ * A class that wraps up our 2D platforming player logic. It creates, animates and moves a sprite in
+ * response to WASD/arrow keys. Call its update method from the scene's update and call its destroy
+ * method when you're done with the player.
+ */
+export default class Player {
+
+    private scene: Phaser.Scene = null;
+    private sprite: Phaser.Physics.Arcade.Sprite = null;
+    private keys = null;
+    private doubleJump = true;
+    private lastJumpTime: number = 0;
+
+    constructor(scene: Phaser.Scene, x: number, y: number) {
+        this.scene = scene;
+
+        this.createAnims(this.scene.anims);
+
+        // Create the physics-based sprite that we will move around and animate
+        this.sprite = scene.physics.add
+            .sprite(x, y, "player", 0)
+            .setDrag(1000, 0)
+            .setMaxVelocity(300, 400)
+            .setSize(18, 24)
+            .setOffset(7, 9);
+
+        // Track the arrow keys & WASD
+        const { LEFT, RIGHT, UP, W, A, D } = Phaser.Input.Keyboard.KeyCodes;
+        this.keys = scene.input.keyboard.addKeys({
+            left: LEFT,
+            right: RIGHT,
+            up: UP,
+            w: W,
+            a: A,
+            d: D
+        });
+    }
+
+    freeze() {
+        const body = this.sprite.body as Phaser.Physics.Arcade.Body;
+        body.moves = false;
+    }
+
+    update(time, delta) {
+        const onGround = this.sprite.body.blocked.down;
+        const acceleration = onGround ? 600 : 200;
+
+        // Apply horizontal acceleration when left/a or right/d are applied
+        if (this.keys.left.isDown || this.keys.a.isDown) {
+            this.sprite.setAccelerationX(-acceleration);
+            // No need to have a separate set of graphics for running to the left & to the right. Instead
+            // we can just mirror the this.sprite.
+            this.sprite.setFlipX(true);
+        } else if (this.keys.right.isDown || this.keys.d.isDown) {
+            this.sprite.setAccelerationX(acceleration);
+            this.sprite.setFlipX(false);
+        } else {
+            this.sprite.setAccelerationX(0);
+        }
+
+        // Only allow the player to jump if they are on the ground
+        if ((onGround || this.doubleJumpAvailable(time)) && (this.keys.up.isDown || this.keys.w.isDown)) {
+            this.sprite.setVelocityY(-500);
+            this.lastJumpTime = time;
+            if (!onGround) {
+                this.doubleJump = false;
+            }
+        }
+
+        // Update the animation/texture based on the state of the player
+        if (onGround) {
+            this.doubleJump = true;
+            if (this.sprite.body.velocity.x !== 0) this.sprite.anims.play("player-run", true);
+            else this.sprite.anims.play("player-idle", true);
+        } else {
+            this.sprite.anims.stop();
+            this.sprite.setTexture("player", 10);
+        }
+    }
+
+    destroy() {
+        this.sprite.destroy();
+    }
+
+    // Create the animations we need from the player spritesheet
+    private createAnims(anims: Phaser.Animations.AnimationManager): any {
+        anims.create({
+            key: "player-idle",
+            frames: anims.generateFrameNumbers("player", { start: 0, end: 3 }),
+            frameRate: 3,
+            repeat: -1
+        });
+        anims.create({
+            key: "player-run",
+            frames: anims.generateFrameNumbers("player", { start: 8, end: 15 }),
+            frameRate: 12,
+            repeat: -1
+        });
+    }
+
+    doubleJumpAvailable(time: number) {
+        return this.doubleJump && (time - this.lastJumpTime> 200);
+    }
+}
